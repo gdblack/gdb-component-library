8090c9007a45f70bc8b598a12cafbcd9a493d2ce
gdblack <gdblackx@me.com>
2025-10-15 11:37:35 -0400

    fix: remove unnecessary cmd from build script and remove unnecessary files

--- src/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary' | 'outline'
-  size?: 'small' | 'medium' | 'large'
-}
-export declare const Button: React.FC<ButtonProps>
--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import './index.css'
-export { Button } from './components/Button'
-export type { ButtonProps } from './components/Button'
